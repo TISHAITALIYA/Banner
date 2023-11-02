eeb8ceef48c3d5960cee5e6253e1d7dbc141832a
italiyatisha <italiyatisha88@gmail.com>
2023-11-02 13:50:37 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+import './App.css';
+import Banner from './Banner';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Banner/> 
+    </div>
+  );
+}
+
+export default App;
